8bc20009bedcf693d8af37295716db2569714881
Björn Dahlgren <bjodah@gmail.com>
2013-12-25 19:45:08 +0100

    Started to refactor setup

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@
 
 from distutils.core import setup
 
+name_ = 'pycompilation'
+version_ = '0.2.2'
+
 setup(
-    name='pycompilation',
-    version='0.2.1',
-    description='Python package for codegeneration and compilation (meta programming).',
+    name=name_,
+    version=version_,
     author='Björn Dahlgren',
     author_email='bjodah@DELETEMEgmail.com',
-    url='https://github.com/bjodah/pycompilation',
+    description='Python package for codegeneration and compilation (meta programming).',
+    license = "BSD",
+    url='https://github.com/bjodah/'+name_,
+    download_url='https://github.com/bjodah/'+name_+'/archive/v'+version_+'.tar.gz',
     packages=['pycompilation'],
 )
