3691380bc5b2d1ad8da28a66a102af7e6bf76419
Björn Dahlgren <bjodah@gmail.com>
2014-01-17 19:07:01 +0100

    Added classifiers in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,20 @@ import pycompilation
 
 name_ = 'pycompilation'
 
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: POSIX",
+    "Programming Language :: Python",
+    "Programming Language :: C",
+    "Programming Language :: Cython",
+    "Programming Language :: Fortran",
+    "Topic :: Software Development :: Code Generators",
+    "Topic :: Software Development :: Compilers",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
 setup(
     name=name_,
     version=pycompilation.__version__,
@@ -16,4 +30,5 @@ setup(
     url='https://github.com/bjodah/'+name_,
     download_url='https://github.com/bjodah/'+name_+'/archive/v'+pycompilation.__version__+'.tar.gz',
     packages=[name_],
+    classifiers = classifiers
 )
