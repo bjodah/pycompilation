34b79ed9689292e633be871433945444403feeac
Björn Dahlgren <bjodah@gmail.com>
2014-02-02 15:18:47 +0100

    [doc] clean up cimment (trigger CI)

--- examples/tests/test_examples.py
+++ examples/tests/test_examples.py
@@ -9,7 +9,7 @@ def run_example(pypath):
     p = subprocess.Popen(
         ['python', pypath, 'clean'],
         cwd=os.path.join(os.path.dirname(__file__),'..'))
-    assert p.wait() == 0 # systems which have `make` have SUCCESS==0
+    assert p.wait() == 0 # SUCCESS==0
 
 
 def test_elemwise():
