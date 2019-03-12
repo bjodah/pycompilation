c5a8ac1fb616bbfa45f95b3451e9104c925c2d06
Björn Dahlgren <bjodah@gmail.com>
2019-03-12 15:56:45 +0100

    Release docs for v0.4.5

--- pycompilation/tests/test_util.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function, division, absolute_import
-
-from pycompilation.util import uniquify
-
-
-def test_uniquify():
-    assert uniquify([1, 1, 2, 2]) == [1, 2]
