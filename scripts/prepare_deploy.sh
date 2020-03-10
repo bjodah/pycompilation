df0f2cf544b13698743f996919cccea8df6e14c1
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-03-10 08:51:00 +0100

    DRONE_BRANCH

--- scripts/prepare_deploy.sh
+++ scripts/prepare_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 touch doc/_build/html/.nojekyll
 cp LICENSE doc/_build/html/
-mkdir -p deploy/public_html/branches/"${CI_BRANCH}" deploy/script_queue
-cp -r dist/* htmlcov/ examples/ doc/_build/html/ deploy/public_html/branches/"${CI_BRANCH}"/
+mkdir -p deploy/public_html/branches/"${DRONE_BRANCH}" deploy/script_queue
+cp -r dist/* htmlcov/ examples/ doc/_build/html/ deploy/public_html/branches/"${DRONE_BRANCH}"/
