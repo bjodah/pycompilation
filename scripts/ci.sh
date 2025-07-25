a3a7a9a537a23f56e0f075d64e025390675ec247
Bjorn <bjodah@gmail.com>
2025-07-25 07:08:29 +0200

    imp -> importlib (#24)
    * imp -> importlib
    
    * tweak tests for new setup
    
    * install sympy if needed during installation, needed by examples/external_lib_sundials.py
    
    * Update CI-config
    
    * Install scipy if needed
    
    * Update ci config
    
    * tweak CI config
    
    * CI tweaks
    
    * fix bash variable syntax

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,13 +1,27 @@
-#!/bin/bash -xeu
-PKG_NAME=${1:-${CI_REPO##*/}}
-if [[ "$DRONE_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
-    eval export ${PKG_NAME^^}_RELEASE_VERSION=\$DRONE_BRANCH
-    echo ${DRONE_BRANCH} | tail -c +2 > __conda_version__.txt
+#!/bin/bash
+set -euxo pipefail
+PKG_NAME=${PKG_NAME:-${CI_REPO_NAME##*/}}
+
+if [[ "$CI_COMMIT_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
+    eval export ${PKG_NAME^^}_RELEASE_VERSION=\$CI_COMMIT_BRANCH
 fi
+
+SUNDIALS_ROOT=$(compgen -G "/opt-3/sundials-6.*-release")
+if [ ! -e $SUNDIALS_ROOT/include/sundials/sundials_config.h ]; then
+    >&2 echo "No sundials installation found at: $SUNDIALS_ROOT"
+    exit 1
+else
+    export PYCOMPILATION_TESTING_SUNDIALS_CFLAGS="-isystem ${SUNDIALS_ROOT}/include"
+    export PYCOMPILATION_TESTING_SUNDIALS_LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,${SUNDIALS_ROOT}/lib -L${SUNDIALS_ROOT}/lib"
+fi
+
+source $(compgen -G /opt-3/cpython-v3.*-apt-deb/bin/activate)
+( set +e; python3 -c "import sympy" || pip install sympy )
+( set +e; python3 -c "import scipy" || pip install scipy )
 python3 setup.py sdist
 (cd dist/; ${PYTHON:-python3} -m pip install pytest $PKG_NAME-$(${PYTHON:-python3} ../setup.py --version).tar.gz)
 (cd /; ${PYTHON:-python3} -m pytest --pyargs $PKG_NAME)
 ${PYTHON:-python3} -m pip install .[all]
-PYTHONPATH=$(pwd) ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
-./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
+PYTHONPATH=$(pwd) ./scripts/run_tests.sh "$@"
+#./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
 ! grep "DO-NOT-MERGE!" -R . --exclude ci.sh
